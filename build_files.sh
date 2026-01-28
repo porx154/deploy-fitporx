b49082a6218b0eaa304502283556bebdb053f0a8
Isaias <porx154@hotmail.com>
2026-01-28 12:42:22 -0500

    una vez mas

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing requirements..."
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3.12 manage.py collectstatic --noinput --clear
